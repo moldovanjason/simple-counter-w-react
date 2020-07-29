3c19ad9603c3916e788a5a08c6146efdd2c1aab5
Gerdeth <gerdethwickham@gmail.com>
2020-07-29 15:21:22 +0000

    update1

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,27 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-export function Home() {
+export function Home(props) {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div>{props.char1} </div>
+			<div>{props.char2}</div>
+			<div>{props.char3}</div>
+			<div>{props.char4}</div>
+			<div>{props.char5}</div>
+			<div>{props.char6}</div>
 		</div>
 	);
 }
+Home.propTypes = {
+	char1: PropTypes.string,
+	char2: PropTypes.string,
+	char3: PropTypes.string,
+	char4: PropTypes.string,
+	char5: PropTypes.string,
+	char6: PropTypes.string
+};
