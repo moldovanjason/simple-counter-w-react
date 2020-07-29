cd14c718ba991e81adec04cd5d19359182ab5911
moldovanjason <moldovanjason@gmail.com>
2020-07-29 18:12:15 +0000

    more

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,7 +7,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 export function Home(props) {
 	return (
-		<div className="text-center mt-5">
+		<div className="timer text-center mt-5 d-flex">
 			<div>{props.char1} </div>
 			<div>{props.char2}</div>
 			<div>{props.char3}</div>
